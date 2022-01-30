f91690aa8c042248c3261ccde3dcdcdfe9abb853
Scott LaBelle <scott.labelle@snhu.edu>
2022-01-30 17:33:15 -0500

    completed reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionText: {
+      type: String,
+      required: true,
+      maxlength: 200,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJson: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
